c9ef1be2b2dd4d5df12ca0f90014c657aab71466
SirDotJ <2020102045@pnu.edu.ru>
2023-11-13 11:33:49 +1000

    Added pointer on elements that can be copied, fixed formatting issue for copied text.

--- js/copyOnClick.js
+++ js/copyOnClick.js
@@ -1,13 +1,13 @@
 /* Copy phone number */
 const phone = document.getElementById("phone");
 const copyPhone = async () => {
-    const number = phone.innerHTML.replaceAll("&nbsp;", " ");
+    const number = phone.innerText.replaceAll(" ", " ");
     await navigator.clipboard.writeText(number);
 }
 
 /* Copy email */
 const email = document.getElementById("email");
 const copyEmail = async () => {
-    const mail = email.innerHTML;
+    const mail = email.innerText;
     await navigator.clipboard.writeText(mail);
 }
