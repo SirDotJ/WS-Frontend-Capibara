5340950489cee1f05212349270b28124084df988
SirDotJ <2020102045@pnu.edu.ru>
2023-11-13 16:14:02 +1000

    MAKE IT STOP MAKE IT STOP

--- js/reminder.js
+++ js/reminder.js
@@ -1,13 +1,15 @@
+const enable = false;
 const timerIntervalMS = 120000;
 
 const reminder = document.getElementById("reminder")
 const showReminder = async() => {
     reminder.classList.toggle("show");
-    console.log("hi there!");
 }
 const hideReminder = async() => {
     reminder.classList.toggle("show");
     setTimeout(showReminder, timerIntervalMS);
 }
 
-setTimeout(showReminder, timerIntervalMS);
+if (enable) {
+    setTimeout(showReminder, timerIntervalMS);
+}
