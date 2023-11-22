28abb2a075811f6ece33e989275c901433b70b8d
SirDotJ <2020102045@pnu.edu.ru>
2023-11-22 21:00:12 +1000

    Added comments to reminder timings.

--- js/reminder.js
+++ js/reminder.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const enable = true;
-const firstIntervalMS = 2000;
-const timerIntervalMS = 120000;
+const firstIntervalMS = 2000; // 2 Seconds
+const timerIntervalMS = 120000; // 2 Minutes (120 seconds)
 
 const reminder = document.getElementById("reminder")
 const showReminder = async() => {
